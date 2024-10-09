9ea3289de0f513a88904d045bb2b4d214c0625d5
amiraatalla <amiraatalla63@gmail.com>
2024-10-09 16:58:00 +0200

    gg

---
+++ asset/js/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById('demo').innerHTML = "hi";
+
+}
+
+function chaneMe() {
+    let ph = document.getElementsByClassName('chang');
+    ph[0].style.color = 'red';
+    // for (let i = 0; i < ph.length; i++) {
+    //     ph[i].style.color = 'red';
+    // }
+}

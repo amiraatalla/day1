ebd4e39ba5213ef0c17508bbaec71c151e99a148
amiraatalla <amiraatalla63@gmail.com>
2024-10-11 15:11:19 +0200

    :sparkles: new feature

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,7 +1,12 @@
 function myFunction() {
-    document.getElementById('demo').innerHTML = "hi";
+    let fName = "amira";
+    let lName = "reda";
+    let age = 20;
+    
+    document.getElementById('demo').innerHTML = age + " "+fName+" "+lName;
 
 }
+
 
 function chaneMe() {
     let ph = document.getElementsByClassName('chang');
@@ -10,3 +15,14 @@ function chaneMe() {
     //     ph[i].style.color = 'red';
     // }
 }
+
+let name = "amira"
+// window.alert(`Hi ${name}!!`)
+let num1 , num2 , result ;
+num1 = 5 
+num2 = 6
+result = num1 + num2
+alert(result )
+alert(typeof(result))
+
+
